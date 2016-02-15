51f04bbec249916abcc2fbcfd3506c79060b20ad
heather39 <heather39@mail.usf.edu>
2016-02-15 03:38:37 -0500

    Second commit to SVN repo.

--- eclipsetosvn.pl
+++ eclipsetosvn.pl
@@ -1,3 +1,6 @@
 #!/usr/bin/perl
 
-print "Heather Theinert"
+print "Heather Theinert\n";
+print "Starting to get SVN working...\n";
+print "This Perl program has been written to test the use\n";
+print "of subversion in Eclipse.";
